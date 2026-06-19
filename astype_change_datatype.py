cb425a03447f21216c25d04797a65a4ec32461c9
Tonmoy Paul <snahasishpaul7@gmail.com>
2026-06-20 01:44:53 +0600

    change the datatype using astype , this is only use in numpy

---
+++ astype_change_datatype.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+arr = np.array([1.2,2.6,3.52])
+
+arr2 = (arr.dtype)
+
+arr3 = arr.astype(int) # we change the datatype by  using astype ,float to int
+
+print(arr)
+
+print(arr2)
+
+print(arr3)
+
+arr4 = (arr3.dtype)
+
+print(arr4)
+
+#astype is like typecasting you can the type of any datatype 
