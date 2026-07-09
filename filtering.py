ce8a0e136e56b2a7dd77349118eda7007f2de513
Tonmoy Paul <snahasishpaul7@gmail.com>
2026-07-10 00:19:23 +0600

    adding filtering using numpy (> < =)

---
+++ filtering.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+arr = np.array([20,30,60,80,200,255,266,200,444])
+
+print(arr[arr>200]) #this will print all the values which are greater than 200
+
+print(arr[arr<200]) #this will print all the values which are less than 200
