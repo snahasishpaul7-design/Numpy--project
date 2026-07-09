b6121e74fc7df4e269ed6f6a5e7a328fc2be68bb
Tonmoy Paul <snahasishpaul7@gmail.com>
2026-07-10 00:06:06 +0600

    adding fancy indexing or selecting multiple array values  at once

---
+++ fanci.py
@@ -0,0 +1,8 @@
+#fancy means selecting multiple element at once using array of indices.
+
+import numpy as np
+
+arr = np.array([20,30,60,80,200,255,266,200,444])
+
+print(arr[[2,3,6]]) #when you use fancy indexing you need to pass the index value in list
+
