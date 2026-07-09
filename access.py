764a1d92f0eb61b147d2a36f4a12b40bc9b1947a
Tonmoy Paul <snahasishpaul7@gmail.com>
2026-07-09 23:47:08 +0600

    adding access using numpy

---
+++ access.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+arr =np.array([20,30,60,80,200])
+
+print(arr[0])
+print(arr[1])
+print(arr[2])
+print(arr[3])
+print(arr[4])
+
+#access work like this you can call any values from the array by 
+# using the index number of that value. 
+# The index number starts from 0 and goes up to n-1 
+# where n is the total number of elements in the array.  
